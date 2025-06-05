442741d0e0026a8e72ca76516ea3fdcd42a258ca
alexmanfait <alex.manfait@ynov.com>
2025-06-05 11:05:06 +0200

    Test pour correctif

--- app/server.js
+++ app/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const userRoutes = require('./backend/routes/userRoutes');
+const userRoutes = require('../backend/routes/userRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
